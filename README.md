0b0fe21484cb69519be1a5a9d37aedb4b10ec1cf
Gabriel Koenig <gabriel@king.li>
2025-12-04 14:36:50 +0100

    Some clean.up

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # office365_tools
 All scripts for office 365 tools to configure sharepoint, outlook, ... 
+
+# Apps Powershell 
+
+# Apps Python
+
+Install the latest python version that is stable (no longer bug fixing). Current version 3.12. 
+Go to apps_python and run 'pip install -e .' to install the required packages. 
--- apps_python/setup.py
+++ apps_python/setup.py
@@ -1,5 +1,5 @@
-## Needed to i 
-## It doesn't copy the code to the standard site-packages directory. Instead, it creates a link to the project's source directory where it currently is.
+## # Run pip install -e . in the \office365_tools\apps_python directory to install the package in editable mode.
+## §It doesn't copy the code to the standard site-packages directory. Instead, it creates a link to the project's source directory where it currently is.
 
 from setuptools import setup, find_packages
 
@@ -7,8 +7,10 @@ setup(
     name="office365_python_tools",
     version="0.1",
     packages=find_packages(),
+    python_requires=">3.10 <3.12", # works for sure with 3.12, not tested yet with lower versions
     install_requires=[
         'azure-identity',
         'msgraph-sdk',
     ]
 )
+
